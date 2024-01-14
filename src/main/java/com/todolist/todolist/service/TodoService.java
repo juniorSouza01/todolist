2ab1a3dee8d9c54e1e4673290620bf1fc964feb9
Gelado047 <juniorthesouza017@gmail.com>
2024-01-14 17:09:57 -0300

    controller

---
+++ src/main/java/com/todolist/todolist/service/TodoService.java
@@ -0,0 +1,38 @@
+package com.todolist.todolist.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import com.todolist.todolist.entity.Todo;
+import com.todolist.todolist.repository.TodoRepository;
+
+@Service
+public class TodoService {
+    private TodoRepository todoRepository;
+
+    public TodoService(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+    public List<Todo> create(Todo todo) {
+        todoRepository.save(todo);
+        return list();
+    }
+
+    public List<Todo> list() {
+        Sort sort = Sort.by(Sort.Order.desc("prioridade"), Sort.Order.asc("nome"));
+        return todoRepository.findAll(sort);
+    }
+
+    public List<Todo> update(Todo todo) {
+        todoRepository.save(todo);
+        return list();
+    }
+
+    public List<Todo> delete(Long id) {
+        todoRepository.deleteById(id);
+        return list();
+    }
+}
